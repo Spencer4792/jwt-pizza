2eb58db94f6e4c66afa07bf01e25ef37313133f5
Spencer Hales <112470587+Spencer4792@users.noreply.github.com>
2025-04-01 23:01:21 -0600

    Create loginAndOrder.js

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,154 @@
+import { sleep, check } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  response = http.put(
+    'https://pizza-service.pizzapalace.click/api/auth',
+    '{"email":"spencerhales4792@gmail.com","password":"password"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.pizzapalace.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  check(response, { 'status equals 200': response => response.status.toString() === '200' })
+
+  vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+  sleep(4.2)
+
+  response = http.get('https://pizza-service.pizzapalace.click/api/order/menu', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"2-l9Fw4VUO7kr8CvBlt4zaMCqXZ0w"',
+      origin: 'https://pizza.pizzapalace.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(1.9)
+
+  response = http.get('https://pizza-service.pizzapalace.click/api/franchise', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"2-l9Fw4VUO7kr8CvBlt4zaMCqXZ0w"',
+      origin: 'https://pizza.pizzapalace.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(3.8)
+
+  response = http.get('https://pizza-service.pizzapalace.click/api/franchise/3', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"2-l9Fw4VUO7kr8CvBlt4zaMCqXZ0w"',
+      origin: 'https://pizza.pizzapalace.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(3.2)
+
+  response = http.get('https://pizza-service.pizzapalace.click/api/order', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.pizzapalace.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(3.2)
+
+  response = http.del('https://pizza-service.pizzapalace.click/api/auth', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.pizzapalace.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+}
